5c971df7be03b6b1a24104d062f4e9e9d9fa87b9
ywz2198 <ywz2198@live.com>
2020-01-03 18:41:20 +0800

    h

--- one.py
+++ one.py
@@ -1,9 +1,28 @@
-from flask import Flask, jsonify,request, url_for, redirect, session, render_template
+from flask import Flask, jsonify,request, url_for, redirect, session, render_template, g
+import sqlite3
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = '123456'
 
+#数据库
+def connect_db(): 
+    sql = sqlite3.connect('data.db')
+    sql.row_factory = sqlite3.Row
+    return sql
+
+def get_db():
+    if not hasattr(g, 'sqlite3'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+#关闭数据库
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+#页面
 @app.route('/')
 def index():
     session.pop('name', None)
@@ -14,13 +33,17 @@ def index():
 @app.route('/home/<name>', methods = ['GET','POST'])
 def home(name):
     session['name'] = name
-    return render_template('home.html',name = name, display=False, mylist = [1,2,3,4],dic = [{'name': 'zack'},{'name': 'zoe'}])
+    db = get_db()
+    cur = db.execute('select id, name, location from users')
+    results = cur.fetchall()
+
+    return render_template('home.html',name = name, display=False, \
+         mylist = [1,2,3,4],dic = [{'name': 'zack'},{'name': 'zoe'}], results=results)
     # return '<h1>hello {}</h1> '.format(name)
 
 
 @app.route('/json')
 def json():
-    mylist = [1,2,3,4]
     if 'name' in session:
         name = session['name']
     else:
@@ -40,6 +63,11 @@ def theform():
     else:
         name = request.form['name']
         location = request.form['location']
+
+        db = get_db()
+        db.execute('insert into users (name, location) values (?, ?)', [name, location])
+        db.commit()
+
         # return  'hello{}, {},submitted'.format(name, location)
         return redirect(url_for('home', name=name,location=location))
 
@@ -58,6 +86,12 @@ def processjson():
     randomlist = data['randomlist']
     return jsonify({'result' : 'success', 'name' : name ,'location' : location, 'randomkeyinlist' : randomlist[1]})
 
+@app.route('/viewresults')
+def viewresults():
+    db = get_db()
+    cur = db.execute('select * from users')
+    results = cur.fetchall()
+    return '<h1>The ID is {}, name{}, location{}</h1>'.format(results[1]['id'], results[1]['name'], results[1]['location'])
 
 if __name__ == '__main__':
     app.run()
